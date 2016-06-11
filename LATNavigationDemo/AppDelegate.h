d33da0eb24034b1b8927f10a0bbadbccaeb83129
KKLater <lshxin89@126.com>
2016-06-11 12:12:04 +0800

    Initial Commit

---
+++ LATNavigationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LATNavigationDemo
+//
+//  Created by Later on 16/6/11.
+//  Copyright © 2016年 Later. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LATNavigationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LATNavigationDemo
+//
+//  Created by Later on 16/6/11.
+//  Copyright © 2016年 Later. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
